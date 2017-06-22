f5e246257a49b017551bffc703b6e26b6b5246b8
deniskyashif <denis.kyashif@gmail.com>
2017-06-22 17:14:48 +0300

    refactored and added new tests for the transducer

--- src/spec/ssfst/ssfst.spec.js
+++ src/spec/ssfst/ssfst.spec.js
@@ -31,7 +31,7 @@ describe('Subsequential Finite State Transducer Tests', () => {
         const actual = transducer.process('a');
         const expected = { accepted: true, output: '1' };
 
-        expect(expected).toEqual(actual);
+        expect(actual).toEqual(expected);
     });
 
     it('Should accept and translate correctly a word from the input dictionary #2', () => {
@@ -39,7 +39,7 @@ describe('Subsequential Finite State Transducer Tests', () => {
         const actual = transducer.process('ab');
         const expected = { accepted: true, output: '2' };
 
-        expect(expected).toEqual(actual);
+        expect(actual).toEqual(expected);
     });
 
     it('Should accept and translate correctly a word from the input dictionary #3', () => {
@@ -47,7 +47,7 @@ describe('Subsequential Finite State Transducer Tests', () => {
         const actual = transducer.process('abcc');
         const expected = { accepted: true, output: '3' };
 
-        expect(expected).toEqual(actual);
+        expect(actual).toEqual(expected);
     });
 
     it('Should accept and translate correctly a word from the input dictionary #4', () => {
@@ -55,7 +55,7 @@ describe('Subsequential Finite State Transducer Tests', () => {
         const actual = transducer.process('babc');
         const expected = { accepted: true, output: '4' };
 
-        expect(expected).toEqual(actual);
+        expect(actual).toEqual(expected);
     });
 
     it('Should accept and translate correctly a word from the input dictionary #5', () => {
@@ -63,7 +63,7 @@ describe('Subsequential Finite State Transducer Tests', () => {
         const actual = transducer.process('c');
         const expected = { accepted: true, output: '5' };
 
-        expect(expected).toEqual(actual);
+        expect(actual).toEqual(expected);
     });
 
     it('Should reject a word that does not belong to the input dictionary #1', () => {
@@ -80,8 +80,22 @@ describe('Subsequential Finite State Transducer Tests', () => {
 
     it('Should reject a word that does not belong to the input dictionary #3', () => {
         const transducer = new SSFST(alphabet, dict);
-        const actual = transducer.process('cc');
+        const actual = transducer.process('dbc');
         expect(actual.accepted).not.toBeTruthy();
-        console.log(actual);
+    });
+
+    it('Should produce a valid output, given a valid input string from the alphabet #1', () => {
+        const transducer = new SSFST(alphabet, dict);
+        const actual = transducer.process('abcbbbabccb');
+
+        const expected = { accepted: true, output: '25bb45b' };
+        expect(expected).toEqual(actual);
+    });
+
+    it('Should reject an input word that contains a symbol that does not belong to the input alphabet #1', () => {
+        const transducer = new SSFST(alphabet, dict);
+        const actual = transducer.process('aXbcbbbabccb');
+
+        expect(actual.accepted).not.toBeTruthy();
     });
 });
